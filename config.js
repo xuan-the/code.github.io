dbc2d9b596f2273e36743ebaf9a574093ccb2ada
xuan-the <93109000+xuan-the@users.noreply.github.com>
2021-10-31 08:45:45 +0700

    upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ngọc';
+const giftUrl = 'https://www.facebook.com/thedzhihi';
+const eventName = 'Giáng sinh vui vẻ';
+const titleCard = 'Tặng Ngọc';
+const contentCard = 'Chúc người tớ yêu giáng sinh tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
